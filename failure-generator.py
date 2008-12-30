7f030a08201b5dc43d77b23ad96ac89d1be334f4
Can Berk Güder <cbguder@gmail.com>
2008-12-31 00:05:26 +0200

    Constraint-based probabilistic failure generation.

--- failure-generator.py
+++ failure-generator.py
@@ -14,30 +14,55 @@ except ImportError:
 
 def main():
 	parser = OptionParser(usage='Usage: %prog [options] CONFIGURATION_MODEL',
-	                      version='%prog 0.2')
-	parser.add_option('-t', '--tests',
-	                  type='int',
-	                  metavar='NUM',
-	                  help='number of tests [default: 2]')
+						  version='%prog 0.2')
 	parser.add_option('-e', '--errors',
-	                  type='int',
-	                  metavar='NUM',
-	                  help='number of errors [default: 2]')
+					  type='int',
+					  metavar='NUM',
+					  help='number of errors [default: 2]')
 	parser.add_option('-f', '--failures',
-	                  type='string',
-	                  action='callback',
-	                  callback=parse_failures,
-	                  help='number and size of failures (e.g. 2x2,3x5) [default: 2x2]')
-	parser.set_defaults(errors=2, tests=2, failures=[(2,2)])
+					  type='string',
+					  action='callback',
+					  callback=parse_failures,
+					  help='number and size of failures (e.g. 2x2,3x5) [default: 2x2]')
+	parser.add_option('--min-failures',
+					  type='int',
+					  metavar='NUM',
+					  action='callback',
+					  callback=failures_callback,
+					  help='minimum failures per test [default: 1]')
+	parser.add_option('--max-failures',
+					  type='int',
+					  metavar='NUM',
+					  action='callback',
+					  callback=failures_callback,
+					  help='maximum failures per test [default: 2]')
+	parser.add_option('--min-coverage',
+					  type='int',
+					  metavar='NUM',
+					  action='callback',
+					  callback=coverage_callback,
+					  help='minimum option coverage [default: 10%]')
+	parser.add_option('--max-coverage',
+					  type='int',
+					  metavar='NUM',
+					  action='callback',
+					  callback=coverage_callback,
+					  help='maximum option coverage [default: 80%]')
+	parser.set_defaults(errors=2,
+						failures=[(2,2)],
+						min_failures=1,
+						max_failures=2,
+						min_coverage=10,
+						max_coverage=80)
 	(options, args) = parser.parse_args()
 
 	if len(args) < 1:
 		parser.print_help()
 		sys.exit()
-	
+
 	configuration_model = ConfigurationModel.from_xml(args[0])
-	
-	failure_model = generate_failures(configuration_model.options, options.tests, options.errors, options.failures)
+
+	failure_model = generate_failures(configuration_model.options, options)
 	output = failure_model.to_xml()
 
 	if use_tidy:
@@ -60,36 +85,121 @@ def parse_failures(option, opt, value, parser):
 			raise OptionValueError('option %s: invalid value: %s' % (opt, value))
 	setattr(parser.values, option.dest, result)
 
-def generate_failures(options, tests, errors, failures):
+def failures_callback(option, opt_str, value, parser):
+	if value < 1:
+		raise OptionValueError('option %s: value < 1' % opt_str)
+
+	if (option.dest == 'min_failures' and value > parser.values.max_failures) or \
+	   (option.dest == 'max_failures' and value < parser.values.min_failures):
+			raise OptionValueError('option min-failures > max-failures')
+
+	setattr(parser.values, option.dest, value)
+
+def coverage_callback(option, opt_str, value, parser):
+	if value < 0:
+		raise OptionValueError('option %s: value < 0' % opt_str)
+
+	if value > 100:
+		raise OptionValueError('option %s: value > 100' % opt_str)
+
+	if (option.dest == 'min_coverage' and value > parser.values.max_coverage) or \
+	   (option.dest == 'max_coverage' and value < parser.values.min_coverage):
+		raise OptionValueError('option min-coverage > max-coverage')
+
+	setattr(parser.values, option.dest, value)
+
+def generate_failures(options, args):
+	tests = None
+	failures = sum([[f[1]] * f[0] for f in args.failures], [])
+
+	# Generate test-failure configuration
+	for P in list_partitions(len(failures), range(args.min_failures, args.max_failures+1)):
+		if tests != None:
+			break
+
+		# 6 is completely arbitrary
+		for i in range(6):
+			temp_tests = slice_list(failures, P)
+			if args.min_coverage <= min_coverage(temp_tests, options) <= args.max_coverage:
+				tests = temp_tests
+				break
+			random.shuffle(failures)
+
+	if tests == None:
+		print "ERROR: Cannot find test configuration."
+		sys.exit()
+
+	# Eliminate some options to make sure we don't exceed maximum option coverage
+	c = len(options)
+	to_eliminate = c - c * args.max_coverage / 100
+	eliminated = random.sample(options, to_eliminate)
+	for option in eliminated:
+		options.remove(option)
+
 	failure_model = FailureModel()
-	for i in range(tests):
+	for i, t in enumerate(tests):
 		test = Test()
 		test.name = 't%d' % (i+1)
-		available_errors = range(1, errors+1)
-		for count,size in failures:
-			for j in range(count):
-				try:
-					picked_options = random.sample(options, size)
-				except:
-					print "ERROR: Number of requested options exceeds the number of available options."
-					sys.exit()
+		available_errors  = range(1, args.errors+1)
+		available_options = options[:]
+		for size in t:
+			try:
+				picked_options = random.sample(available_options, size)
+				for option in picked_options:
+					available_options.remove(option)
+			except:
+				print "ERROR: Number of requested options exceeds the number of available options."
+				sys.exit()
 
-				picked_values  = [random.choice(option.values) for option in picked_options]
-				pattern = FailurePattern()
+			picked_values = [random.choice(option.values) for option in picked_options]
+			pattern = FailurePattern()
 
-				try:
-					error = random.choice(available_errors)
-				except:
-					print "ERROR: Number of requested failures exceeds the number of errors."
-					sys.exit()
+			try:
+				error = random.choice(available_errors)
+			except:
+				print "ERROR: Number of requested failures exceeds the number of errors."
+				sys.exit()
 
-				available_errors.remove(error)
-				pattern.result = 'e%d' % error
-				for k in range(size):
-					pattern.options[picked_options[k].name] = picked_values[k]
-				test.patterns.append(pattern)
+			available_errors.remove(error)
+			pattern.result = 'e%d' % error
+			for k in range(size):
+				pattern.options[picked_options[k].name] = picked_values[k]
+			test.patterns.append(pattern)
 		failure_model.tests.append(test)
 	return failure_model
+
+def partitions(n):
+	"""Integer partitions of n, in reverse lexicographic order."""
+	if n == 0:
+		yield []
+	if n <= 0:
+		return
+	for p in partitions(n-1):
+		if len(p) == 1 or (len(p) > 1 and p[-1] < p[-2]):
+			p[-1] += 1
+			yield p
+			p[-1] -= 1
+		p.append(1)
+		yield p
+		p.pop()
+
+def list_partitions(n, L):
+	sL = set(L)
+	for p in partitions(n):
+		if len(set(p) - sL) == 0:
+			yield p
+
+def min_coverage(tests, options):
+	coverage = max([sum(test) for test in tests])
+	return 100.0 * coverage / len(options)
+
+def slice_list(L, P):
+	result = []
+	i = 0
+	for p in P:
+		result.append(L[i:i+p])
+		i += p
+	return result
 
 if __name__ == '__main__':
 	main()
