fb7be88b218327a6a341e9cc6ad336cba6353c60
Can Berk Güder <cbguder@gmail.com>
2009-01-17 02:06:05 +0200

    Bugfix for final-test convenience scripts.

--- scripts/final-test.py
+++ scripts/final-test.py
@@ -5,16 +5,19 @@ import sys
 from subprocess import Popen
 
 for file in os.listdir('failure_patterns'):
-	if os.path.exists('simulation_results/decision-tree/' + file):
-		print 'SKIP ' + file
-	else:
-		parts = file.split('-')
-		failure_pattern     = 'failure_patterns/' + file
-		configuration_model = 'configuration_models/%s.xml' % '-'.join(parts[:2])
-		covering_array      = 'covering_arrays/%s.csv' % '-'.join(parts[:3])
-		if os.path.exists(configuration_model) and os.path.exists(covering_array):
-				p = Popen(['../../scripts/final-test',
-				           configuration_model,
-				           covering_array,
-				           failure_pattern])
-				p.wait()
+	parts = file.split('-')
+	part = '-'.join(parts[:2])
+	failure_pattern     = 'failure_patterns/' + file
+	configuration_model = 'configuration_models/%s.xml' % part
+	if os.path.exists(configuration_model):
+		for t in range(2, 6):
+			covering_array = 'covering_arrays/%s-%d.csv' % (part, t)
+			if os.path.exists(covering_array):
+				if os.path.exists('simulation_results/decision-tree/%s-%d-%s' % (part, t, '-'.join(parts[2:]))):
+					print 'SKIP ' + file
+				else:
+					p = Popen(['../../scripts/final-test',
+					           configuration_model,
+					           covering_array,
+					           failure_pattern])
+					p.wait()
